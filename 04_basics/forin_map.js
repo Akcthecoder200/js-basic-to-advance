cf96e0002d9cf1831ffc711d67e504262123d7b9
Arvind Kumar <akchoudhary2411@gmail.com>
2024-06-03 23:28:26 +0530

    added loop map,filter and reduce

---
+++ 04_basics/forin_map.js
@@ -0,0 +1,30 @@
+//for in mostly used for objects...
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+   // console.log(programming[key]);
+}
+//for in can also work for array but different format like key and value key gives index position by which we can get its value...
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
+}//this loop not give any output  as map not work on it..

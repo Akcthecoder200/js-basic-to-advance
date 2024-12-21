988d87a43e4e3cbc3e11e4b96baca1cf54996943
Arvind Kumar <akchoudhary2411@gmail.com>
2024-12-21 12:32:26 +0530

    variable changes..

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,5 +1,22 @@
-const ak=12345
-let bkroono="2121321"
-var dk="raju"
-ck="rajasthan"
-console.log();
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
